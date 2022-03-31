032c854d20041493fb4baf521988afcfd7285869
Leralite <joshlera444@gmail.com>
2022-04-01 01:13:47 +0200

    first

--- mergeSort.py
+++ mergeSort.py
@@ -15,9 +15,6 @@ def mergeSortSteps(a,b):
     steps.append([a,mid])
     mergeSortSteps(a,mid)
 
-def mergeSort():
-    return
-
 def merge(a,b):
     global y
 
@@ -45,35 +42,24 @@ def merge(a,b):
 
     y[a:b+1] = np.array(aux)
 
-size = 200
+def mergeSort(i=int):
+    merge(steps[i][0], steps[i][1])
+    fig.clear()
+    p = plt.bar(x,y, align='edge', width=1.01)
+    
+#######################################
+
+size = 1024
 y = np.random.default_rng().uniform(0,1,size)
 x = np.arange(size)
-
-fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12,5))
-axes[0].bar(x,y, align='edge', width=1)
 
 steps = [[0, size-1]]
 mergeSortSteps(0, size-1)
 steps.reverse()
-for step in steps:
-    merge(step[0], step[1])
+print('size: ', len(steps))
 
-axes[1].bar(x,y, align='edge', width=1)
+fig = plt.figure()
+animator = ani.FuncAnimation(fig, mergeSort, frames=len(steps), interval=0.2)
 
-# plt.figure()
-# plt.bar(x,y, align='edge', width=1)
-# plt.title('Random numbers')
-# plt.ylabel('values')
-fig.tight_layout()
-plt.show()
-
-
-
-
-# plt.figure()
-# plt.boxplot(index_stack, whis=1, showfliers=False, widths=1, medianprops=dict(linewidth=0), boxprops=dict(color="red"))
-# plt.title('Merge Sort steps')
-# plt.ylabel('ranges')
-# plt.xticks(np.arange(0,len(index_stack),max(1,len(index_stack)/20)))
-# plt.xlabel('step')
 # plt.show()
+animator.save(r'mergeSort-{}.gif'.format(size))
